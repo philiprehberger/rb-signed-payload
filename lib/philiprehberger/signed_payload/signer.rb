5440ca12e11d3ed6e68f8d7a8baf3854d6c8a142
Philip Rehberger <me@philiprehberger.com>
2021-03-28 23:55:50 -0600

    Standardize RuboCop config and auto-correct offenses

--- lib/philiprehberger/signed_payload/signer.rb
+++ lib/philiprehberger/signed_payload/signer.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-require "openssl"
-require "json"
-require "base64"
+require 'openssl'
+require 'json'
+require 'base64'
 
 module Philiprehberger
   module SignedPayload
     class Signer
       ALGORITHMS = {
-        sha256: "SHA256",
-        sha384: "SHA384",
-        sha512: "SHA512"
+        sha256: 'SHA256',
+        sha384: 'SHA384',
+        sha512: 'SHA512'
       }.freeze
 
       def initialize(key:, algorithm: :sha256)
@@ -41,22 +41,22 @@ module Philiprehberger
       def decode(token)
         encoded, _sig = split_token(token)
         parsed = JSON.parse(Base64.urlsafe_decode64(encoded))
-        parsed["data"]
+        parsed['data']
       rescue JSON::ParserError
-        raise MalformedToken, "invalid payload encoding"
+        raise MalformedToken, 'invalid payload encoding'
       end
 
       private
 
       def build_payload(data, expires_in)
-        hash = { "data" => data }
-        hash["exp"] = Time.now.to_i + expires_in if expires_in
+        hash = { 'data' => data }
+        hash['exp'] = Time.now.to_i + expires_in if expires_in
         JSON.generate(hash)
       end
 
       def split_token(token)
-        parts = token.to_s.split(".")
-        raise MalformedToken, "invalid token format" unless parts.length == 2
+        parts = token.to_s.split('.')
+        raise MalformedToken, 'invalid token format' unless parts.length == 2
 
         parts
       end
@@ -64,23 +64,23 @@ module Philiprehberger
       def verify_signature!(encoded, sig)
         expected = compute_signature(encoded)
         actual = Base64.urlsafe_decode64(sig)
-        raise InvalidSignature, "signature mismatch" unless secure_compare(expected, actual)
+        raise InvalidSignature, 'signature mismatch' unless secure_compare(expected, actual)
       rescue ArgumentError
-        raise InvalidSignature, "signature mismatch"
+        raise InvalidSignature, 'signature mismatch'
       end
 
       def decode_payload(encoded)
         parsed = JSON.parse(Base64.urlsafe_decode64(encoded))
         check_expiration!(parsed)
-        parsed["data"]
+        parsed['data']
       rescue JSON::ParserError
-        raise MalformedToken, "invalid payload encoding"
+        raise MalformedToken, 'invalid payload encoding'
       end
 
       def check_expiration!(parsed)
-        return unless parsed.key?("exp")
+        return unless parsed.key?('exp')
 
-        raise ExpiredToken, "token has expired" if parsed["exp"] <= Time.now.to_i
+        raise ExpiredToken, 'token has expired' if parsed['exp'] <= Time.now.to_i
       end
 
       def validate_algorithm!(algorithm)
